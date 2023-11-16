a1086adb10f2d2e0c6f03b59835982974653b12a
Bojan Mihelac <bmihelac@mihelac.org>
2023-11-16 19:09:26 +0100

    fix: convert RGBA images to RGB for Pillow < 10
    ```python
    from PIL import Image, ImageChops
    i1 = Image.new('RGBA', (100, 100), 'white')
    i2 = Image.new('RGBA', (100, 100), 'black')
    assert ImageChops.difference(i1, i2).getbbox() is not None, "bounding box should not be None"
    ```

--- pytest_image_snapshot.py
+++ pytest_image_snapshot.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytest
 from PIL import Image, ImageChops
+from PIL import __version__ as PIL_VERSION
 
 
 class ImageMismatchError(AssertionError):
@@ -14,13 +15,12 @@ def pytest_addoption(parser):
     )
 
 
-def extend_to_match_size_rgba(img1, img2):
+def extend_to_match_size(img1, img2):
     """
-    Extend the smaller image to match the size of the larger one using RGBA mode
-    with a transparent background.
+    Extend the smaller image to match the size of the larger one.
 
-    :param img1: First PIL Image object in RGBA mode
-    :param img2: Second PIL Image object in RGBA mode
+    :param img1: First PIL Image object
+    :param img2: Second PIL Image object
     :return: Tuple of two PIL Image objects with the same size
     """
     max_width = max(img1.width, img2.width)
@@ -29,11 +29,20 @@ def extend_to_match_size_rgba(img1, img2):
     def extend_image(img):
         if img.width == max_width and img.height == max_height:
             return img
-        new_img = Image.new("RGBA", (max_width, max_height), (0, 0, 0, 0))
-        new_img.paste(img, (0, 0), img)
+        new_img = Image.new(img.mode, (max_width, max_height), (0, 0, 0, 0))
+        new_img.paste(img, (0, 0))
         return new_img
 
-    return extend_image(img1.convert("RGBA")), extend_image(img2.convert("RGBA"))
+    return extend_image(img1), extend_image(img2)
+
+
+def image_diff(img_1, img_2):
+    # PIL < 10 doesn not have alpha_only getbbox argument
+    if int(PIL_VERSION.split(".")[0]) <= 10:
+        diff = ImageChops.difference(img_1.convert("RGB"), img_2.convert("RGB"))
+        return diff if diff.getbbox() else None
+    diff = ImageChops.difference(img_1, img_2)
+    return diff if diff.getbbox(alpha_only=False) else None
 
 
 @pytest.fixture
@@ -45,9 +54,9 @@ def image_snapshot(request):
         img_path = Path(img_path)
         if not update_snapshots and img_path.exists():
             src_image = Image.open(img_path)
-            img_1, img_2 = extend_to_match_size_rgba(img, src_image)
-            diff = ImageChops.difference(img_1, img_2)
-            if diff.getbbox():
+            img_1, img_2 = extend_to_match_size(img, src_image)
+            diff = image_diff(img_1, img_2)
+            if diff:
                 if config.option.verbose:
                     diff.show(title="diff")
                     if config.option.verbose > 1:
